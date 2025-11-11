7064176ac174f2b25be30c28e5d475bfee72f16d
amirhossein esalatfar <sltamirhosein@gmail.com>
2025-11-11 10:22:20 +0330

    add final files to my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
 import Input from "./Input";
 function App() {
-  const handleChange = (event) => {
-    const [searchTerm, setSearchTerm] = useState("");
-    setSearchTerm(event.target.value);
-  };
-  return <Input/>
+  return <Input />;
 }
 
 export default App;
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,16 +1,41 @@
 import { useState } from "react";
+import cities from "./cities.json";
 
-const Input = ({ handleChange, hint }) => {
-  const [cites, setCites] = useState("");
-  const filterCity = ()=>{
-  
-  }
+const Input = ({ hint }) => {
+  const [city, setCity] = useState(cities);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredCities, setFilteredCities] = useState([]);
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+    const result = cities.filter((city) =>
+      city.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredCities(result);
+  };
+  const handleSelect = (city) => {
+    setSearchTerm(city);
+    setFilteredCities([]);
+  };
   return (
     <div className="input">
       <label htmlFor="input">{hint}</label>
-      <input type="text" id="input" onChange={handleChange} />
+      <input
+        type="text"
+        id="input"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+
+      <ul>
+        {filteredCities.length > 0 &&
+          filteredCities.map((city, index) => (
+            <li key={index} onClick={() => handleSelect(city)}>
+              {city}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 };
-
 export default Input;
